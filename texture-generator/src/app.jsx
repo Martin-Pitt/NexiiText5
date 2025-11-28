a6b05bddad69861a4efc8d49d5de151aecd9b36e
Martin Pitt <martin.r.pitt@gmail.com>
2025-11-28 15:07:12 +0000

    Match router to base path as well

--- texture-generator/src/app.jsx
+++ texture-generator/src/app.jsx
@@ -4,11 +4,11 @@ import Landing from './pages/Landing';
 const Generator = lazy(() => import('./pages/Generator'));
 
 export const App = () => (
-	<LocationProvider>
+	<LocationProvider scope="/NexiiText5/">
 		<ErrorBoundary>
 			<Router>
-				<Route path="/" component={Landing}/>
-				<Route path="/generator" component={Generator}/>
+				<Route path="/NexiiText5/" component={Landing}/>
+				<Route path="/NexiiText5/generator" component={Generator}/>
 			</Router>
 		</ErrorBoundary>
 	</LocationProvider>
